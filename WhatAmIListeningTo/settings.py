fd12fa36a79cb99efba1ad2d4caf2a8419bf5259
Vadim.Zhyvylo <jyvylo5@gmail.com>
2018-02-24 23:56:17 +0200

    Added initialising of database setting with dj_database_url

--- WhatAmIListeningTo/settings.py
+++ WhatAmIListeningTo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,10 +76,8 @@ WSGI_APPLICATION = 'WhatAmIListeningTo.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_URL')
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"))
 }
 
 
